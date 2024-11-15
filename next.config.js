b032b646492f0f98db441fcffdfd0b58055f8f00
AryanShelke <aryan.shelke.2003@gmail.com>
2024-11-15 01:05:56 -0800

    Adjust proxy to reach Flask backend

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/python/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5328/api/:path*"
+            ? "http://127.0.0.1:5328/api/python/:path*"
             : "/api/",
       },
     ];
