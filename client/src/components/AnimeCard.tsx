00a37cdd7c58011231fed738ef6ce4f5eab1ef57
AryanShelke <aryan.shelke.2003@gmail.com>
2024-11-16 17:45:43 -0800

    Readd MAL links for anime cards

--- client/src/components/AnimeCard.tsx
+++ client/src/components/AnimeCard.tsx
@@ -36,36 +36,42 @@ export default function AnimeCard({
       <Card
         className={`flex h-full flex-col bg-white hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600`}
       >
-        <CardBody className="overflow-visible p-0">
-          <Image
-            shadow="sm"
-            width="100%"
-            alt={recommendation.name}
-            height={300}
-            className="w-full rounded-b-none object-cover max-sm:h-full"
-            src={recommendation.image}
-          />
-        </CardBody>
-        <CardFooter className="m-flex flex-col px-4">
-          <h5 className="mb-2 line-clamp-1 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {recommendation.name}
-          </h5>
-          <div className="flex flex-row justify-between gap-4">
-            <p
-              className={`mb-3 font-normal ${getScoreColorClass(recommendation.score)}`}
-            >
-              Score: {recommendation.score.toFixed(2)}/10
+        <Link
+          className="flex flex-col"
+          href={recommendation.url}
+          target="_blank"
+        >
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              width="100%"
+              alt={recommendation.name}
+              height={300}
+              className="w-full rounded-b-none object-cover max-sm:h-full"
+              src={recommendation.image}
+            />
+          </CardBody>
+          <CardFooter className="m-flex flex-col px-4">
+            <h5 className="mb-2 line-clamp-1 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {recommendation.name}
+            </h5>
+            <div className="flex flex-row justify-between gap-4">
+              <p
+                className={`mb-3 font-normal ${getScoreColorClass(recommendation.score)}`}
+              >
+                Score: {recommendation.score.toFixed(2)}/10
+              </p>
+              <p className="text-gray-900 dark:text-white">
+                Popularity: #{recommendation.popularity}
+              </p>
+            </div>
+            <p className="line-clamp-5 text-sm text-gray-400">
+              {recommendation.synopsis.length > 250
+                ? recommendation.synopsis.slice(0, 250) + "..."
+                : recommendation.synopsis}
             </p>
-            <p className="text-gray-900 dark:text-white">
-              Popularity: #{recommendation.popularity}
-            </p>
-          </div>
-          <p className="line-clamp-5 text-sm text-gray-400">
-            {recommendation.synopsis.length > 250
-              ? recommendation.synopsis.slice(0, 250) + "..."
-              : recommendation.synopsis}
-          </p>
-        </CardFooter>
+          </CardFooter>
+        </Link>
       </Card>
       {/* <CardButtons animeLink={recommendation.url} /> */}
     </div>
