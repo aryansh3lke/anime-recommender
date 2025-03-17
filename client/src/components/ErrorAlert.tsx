354b16f7d721c13a5b0573f28a92d62553078d72
AryanShelke <aryan.shelke.2003@gmail.com>
2025-03-16 22:37:58 -0700

    Add solid background for error alerts

--- client/src/components/ErrorAlert.tsx
+++ client/src/components/ErrorAlert.tsx
@@ -3,7 +3,7 @@ import { Alert } from "@heroui/alert";
 export default function ErrorAlert({ message }: { message: string }) {
   return (
     <div className="my-3 flex w-full items-center">
-      <Alert color="danger" title={message} />
+      <Alert color="danger" variant="solid" title={message} />
     </div>
   );
 }
